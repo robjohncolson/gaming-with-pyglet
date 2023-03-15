9f3c303c135f11488b707298e64720c57b5104f8
Robert Colson <robjohncolson@protonmail.com>
2023-03-14 23:39:43 -0500

    added a down button feature, playing with text function

--- game.py
+++ game.py
@@ -28,7 +28,6 @@ def center_anchor(img):
     img.anchor_x = img.width // 2
     img.anchor_y = img.height // 2
 
-    
 box_image_on = pyglet.resource.image('box_on.png')
 center_anchor(box_image_on)
 box_image_right = pyglet.resource.image('box_right.png')
@@ -37,15 +36,12 @@ box_image_left = pyglet.resource.image('box_left.png')
 center_anchor(box_image_left)
 box_image = pyglet.resource.image('box.png')
 center_anchor(box_image)
+box_image_off = pyglet.resource.image('box_off.png')
+center_anchor(box_image_off)
 center_x = int(win.width/2)
 center_y = int(win.height/2)
 
 
-label = Label('hey you guys!',
-              font_name='Times New Roman',
-              font_size=256,
-              x=center_x, y=center_y,
-              anchor_x='center', anchor_y='center')
 
 
 
@@ -61,12 +57,17 @@ class Box(Sprite):
             self.right = False
             self.jump = False
             self.left = False
+            self.duck = False
 
         def update(self, dt):
             self.image = box_image
             
             if self.jump:
                 self.image = box_image_on
+                self.dy = 60 + self.dy
+            if self.duck:
+                self.image = box_image_off
+                self.dy = self.dy - 60
             if self.right:
                 self.image = box_image_right
                 self.x = 60 + self.x
@@ -81,14 +82,20 @@ class Box(Sprite):
 
 def update(dt): # ... create a scheduled function called 'update' which accepts paramter 'dt' (delta time/change in time)
     box.update(dt)
-    
-# ... call the update function once per second.
-pyglet.clock.schedule_interval(update, 1/2.0)
+# ... call the update function twice per second.
+pyglet.clock.schedule_interval(update, 1/60.0)
 
 # ... create a 'box' instance of a Sprite object using the Box constructor
 box = Box(box_image,
             x=center_x + 640, y=center_y,
             dx=0, dy=140)
+
+speed = str(box.dy)
+ 
+label = Label(speed, font_name='Times New Roman',
+              font_size=256,
+              x=center_x, y=center_y,
+              anchor_x='center', anchor_y='center')
 
 @win.event #Event handler which sets variables when keyboard is pressed
 def on_key_press(symbol, modifiers):
@@ -98,6 +105,8 @@ def on_key_press(symbol, modifiers):
         box.right = True
     if symbol == key.LEFT:
         box.left = True
+    if symbol == key.DOWN:
+        box.duck = True
   
 @win.event #Event handler which sets variables when keyboard is released
 def on_key_release(symbol, modifiers):
@@ -107,11 +116,13 @@ def on_key_release(symbol, modifiers):
         box.right = False
     if symbol == key.LEFT:
         box.left = False
+    if symbol == key.DOWN:
+        box.duck = False
   
 @win.event #Event handler which clears teh window, draws ship & planet when on_draw occurs 
 def on_draw():
     # ... drawing code ....
-    #win.clear() # ... call the clear() method of the Window constructor instance named 'win'
+    win.clear() # ... call the clear() method of the Window constructor instance named 'win'
     box.draw() # ... call the draw() method of the Ship constructor (subclass of Sprite, a submodule of sprite,) instance named 'ship'
     label.draw() # ... call the draw() method of the label instance of the Label constructor, a class of submodule text of module pyglet.
     
