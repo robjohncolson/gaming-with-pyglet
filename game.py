0d420a18430c9441b9c09ea9d432a50aadef65b2
Robert Colson <robjohncolson@protonmail.com>
2023-03-15 22:09:36 -0500

    f-ed around

--- game.py
+++ game.py
@@ -8,6 +8,9 @@ from pyglet.text import Label # import Label constructor, a class from pyglet.te
 import pyglet
 import os
 import math
+from random import randint
+from pyglet.gl import glClearColor
+from pyglet.graphics import draw
 
 
 win = Window(width=None, height=None, caption=None, resizable=False, style=None, fullscreen=True, visible=True, vsync=True, file_drops=False, display=None, screen=None, config=None, context=None, mode=None) # call Window constructor to create a fullscreen window whose name is win
@@ -15,14 +18,6 @@ win = Window(width=None, height=None, caption=None, resizable=False, style=None,
 pyglet.resource.reindex()
 
 
-def wrap(value, width):
-    if width == 0:
-        return 0
-    if value > width:
-        value -= width
-    if value < 0:
-        value += width
-    return value
 
 def center_anchor(img):
     img.anchor_x = img.width // 2
@@ -40,9 +35,19 @@ box_image_off = pyglet.resource.image('box_off.png')
 center_anchor(box_image_off)
 center_x = int(win.width/2)
 center_y = int(win.height/2)
+width = int(win.width)
+height = int(win.height)
 
 
 
+def wrap(value, width):
+    if width == 0:
+        return 0
+    if value > width:
+        value -= width
+    if value < 0:
+        value += width
+    return value
 
 
 class Box(Sprite):
@@ -60,42 +65,41 @@ class Box(Sprite):
             self.duck = False
 
         def update(self, dt):
-            self.image = box_image
             
             if self.jump:
                 self.image = box_image_on
-                self.dy = 60 + self.dy
-            if self.duck:
+                self.y = 40 + self.y
+                self.dy = 1 + self.dy
+            elif self.duck:
                 self.image = box_image_off
-                self.dy = self.dy - 60
-            if self.right:
+                self.dy = self.dy - 11
+                self.y = self.y - 40
+            elif self.right:
                 self.image = box_image_right
-                self.x = 60 + self.x
-            if self.left:
+                self.x = 240 + self.x
+                self.dx = 1 + self.dx
+            elif self.left:
                 self.image = box_image_left
-                self.x = self.x - 60
+                self.x = self.x - 240
+                self.dx = self.dx - 1
+            else:
+                self.image = box_image
+
             self.x += self.dx * dt
             self.x = wrap(self.x, win.width)
-            self.y += self.dy * dt
+            self.y = self.y + (self.dy * dt - 9.8 * dt)
             self.y = wrap(self.y, win.height)
 
 
 def update(dt): # ... create a scheduled function called 'update' which accepts paramter 'dt' (delta time/change in time)
     box.update(dt)
 # ... call the update function twice per second.
-pyglet.clock.schedule_interval(update, 1/60.0)
+pyglet.clock.schedule_interval(update, 1/4.0)
 
 # ... create a 'box' instance of a Sprite object using the Box constructor
 box = Box(box_image,
             x=center_x + 640, y=center_y,
-            dx=0, dy=140)
-
-speed = str(box.dy)
- 
-label = Label(speed, font_name='Times New Roman',
-              font_size=256,
-              x=center_x, y=center_y,
-              anchor_x='center', anchor_y='center')
+            dx=0, dy=0)
 
 @win.event #Event handler which sets variables when keyboard is pressed
 def on_key_press(symbol, modifiers):
@@ -118,11 +122,17 @@ def on_key_release(symbol, modifiers):
         box.left = False
     if symbol == key.DOWN:
         box.duck = False
-  
+        
+label = Label("hello, world.", font_name='Times New Roman',
+              font_size=96,
+              x=center_x, y=center_y,
+              anchor_x='center', anchor_y='center')
+
+glClearColor(.1, .045, .06, 0.0) 
 @win.event #Event handler which clears teh window, draws ship & planet when on_draw occurs 
 def on_draw():
     # ... drawing code ....
-    win.clear() # ... call the clear() method of the Window constructor instance named 'win'
+    #win.clear() # ... call the clear() method of the Window constructor instance named 'win'
     box.draw() # ... call the draw() method of the Ship constructor (subclass of Sprite, a submodule of sprite,) instance named 'ship'
     label.draw() # ... call the draw() method of the label instance of the Label constructor, a class of submodule text of module pyglet.
     
